6981f8db6045000cdbd5467ddba9181d9e1d50b1
notargiacomo.s <notargiacomo.s@589bf8a6-3551-0410-881f-c5c050bba8b5>
2008-07-17 17:58:19 +0000

    aggiunte behaviour

--- trunk/SLAsh/src/slash/resource/agent/MemoryAgent.java
+++ trunk/SLAsh/src/slash/resource/agent/MemoryAgent.java
@@ -1,5 +1,8 @@
 package slash.resource.agent;
 
+import slash.df.DFUtil;
+import slash.resource.behaviour.*;
+import jade.core.AID;
 import jade.core.Agent;
 
 public class MemoryAgent extends Agent {
@@ -7,6 +10,16 @@ public class MemoryAgent extends Agent {
 	private static final long serialVersionUID = -5491334259558404278L;
 
 	protected void setup() {
+		System.out.println("Memory: "+this.getName());
+
+		DFUtil.register(this, this.getLocalName(), "resource");
 		
+		AID cmAid = DFUtil.search(this, "cm"+this.getName().charAt(2), "context-manager");
+		this.addBehaviour(new MemoryBehaviour(cmAid));
+
+	}
+	
+	protected void takeDown() {
+		DFUtil.deregister(this, this.getLocalName(), "resource");
 	}
 }
