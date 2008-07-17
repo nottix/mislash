c51f0d96d45937204cd56bc75108293cd1737c71
notargiacomo.s <notargiacomo.s@589bf8a6-3551-0410-881f-c5c050bba8b5>
2008-07-17 13:50:43 +0000

    Aggiunti agenti delle risorse (cpu, ram ...)

---
+++ trunk/SLAsh/src/slash/resource/agent/LatencyAgent.java
@@ -0,0 +1,12 @@
+package slash.resource.agent;
+
+import jade.core.Agent;
+
+public class LatencyAgent extends Agent {
+
+	private static final long serialVersionUID = -5626212945073184408L;
+
+	protected void setup() {
+		
+	}
+}
---
+++ trunk/SLAsh/src/slash/resource/agent/MemoryAgent.java
@@ -0,0 +1,12 @@
+package slash.resource.agent;
+
+import jade.core.Agent;
+
+public class MemoryAgent extends Agent {
+
+	private static final long serialVersionUID = -5491334259558404278L;
+
+	protected void setup() {
+		
+	}
+}
