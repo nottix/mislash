3706065547c84c7e2cda8b9f00c19b009a069827
notargiacomo.s <notargiacomo.s@589bf8a6-3551-0410-881f-c5c050bba8b5>
2008-07-17 13:03:22 +0000

    Realizzata entità context

--- trunk/SLAsh/src/slash/entity/Context.java
+++ trunk/SLAsh/src/slash/entity/Context.java
@@ -5,5 +5,62 @@ import java.io.Serializable;
 public class Context implements Serializable {
 
 	private static final long serialVersionUID = -4290838847903200791L;
+	
+	public static int WIRED = 0;
+	public static int WIRELESS = 1;
+	
+	private float cpu;
+	private float ram;
+	private float memory;
+	private int network; //WIRED, WIRELESS
+	private int bandwidth;
+	private float energy;
+	
+	public void setCpu(float cpu) {
+		this.cpu = cpu;
+	}
+	
+	public float getCpu() {
+		return this.cpu;
+	}
 
+	public void setRam(float ram) {
+		this.ram = ram;
+	}
+	
+	public float getRam() {
+		return this.ram;
+	}
+	
+	public void setMemory(float memory) {
+		this.memory = memory;
+	}
+	
+	public float getMemory() {
+		return this.memory;
+	}
+	
+	public void setNetwork(int network) {
+		this.network = network;
+	}
+	
+	public int getNetwork() {
+		return this.network;
+	}
+	
+	public void setBandwidth(int bandwidth) {
+		this.bandwidth = bandwidth;
+	}
+	
+	public int getBandwidth() {
+		return this.bandwidth;
+	}
+	
+	public void setEnergy(float energy) {
+		this.energy = energy;
+	}
+	
+	public float getEnergy() {
+		return this.energy;
+	}
 }
