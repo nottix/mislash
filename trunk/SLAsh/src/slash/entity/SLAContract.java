465d73f5959fae861594dfbbfbc01139137c02cf
notargiacomo.s <notargiacomo.s@589bf8a6-3551-0410-881f-c5c050bba8b5>
2008-07-18 20:56:18 +0000

    implementato meccanismo di connessione tra publisher e subscriber; lo slacheker riceve i contratti delle coppie pub/sub

--- trunk/SLAsh/src/slash/entity/SLAContract.java
+++ trunk/SLAsh/src/slash/entity/SLAContract.java
@@ -1,25 +1,48 @@
 package slash.entity;
 
 import java.io.Serializable;
+import jade.core.*;
 
 public class SLAContract implements Serializable {
 
 	private static final long serialVersionUID = 1280366527966115376L;
 	
+	private AID publisher;
+	private AID subscriber;
 	private int latency;
 	private float reliability;
 	private int reqInterval;
 	
-	public SLAContract(int latency, float reliability, int reqInterval) {
+	public SLAContract(AID publisher, AID subscriber, int latency, float reliability, int reqInterval) {
+		this.publisher = publisher;
+		this.subscriber = subscriber;
 		this.latency = latency;
 		this.reliability = reliability;
 		this.reqInterval = reqInterval;
 	}
 	
 	public SLAContract() {
+		this.publisher = null;
+		this.subscriber = null;
 		this.latency = 0;
 		this.reliability = 0.0f;
 		this.reqInterval = 0;
+	}
+	
+	public void setPublisher(AID publisher) {
+		this.publisher = publisher;
+	}
+	
+	public AID getPublisher() {
+		return this.publisher;
+	}
+	
+	public void setSubscriber(AID subscriber) {
+		this.subscriber = subscriber;
+	}
+	
+	public AID getSubscriber() {
+		return this.subscriber;
 	}
 	
 	public void setLatency(int latency) {
--- trunk/SLAsh/src/slash/slachecker/agent/SLACheckerAgent.java
+++ trunk/SLAsh/src/slash/slachecker/agent/SLACheckerAgent.java
@@ -6,6 +6,8 @@ package slash.slachecker.agent;
 import jade.core.*;
 import slash.slachecker.behaviour.*;
 import slash.df.*;
+import java.util.*;
+import slash.entity.*;
 
 /**
  * @author Simone Notargiacomo, Lorenzo Tavernese, Ibrahim Khalili
@@ -15,10 +17,18 @@ public class SLACheckerAgent extends Agent {
 
 	private static final long serialVersionUID = -7918542991436312908L;
 	
+	private LinkedList<SLAContract> contractList = null;
+	
+	public LinkedList<SLAContract> getContractList() {
+		return this.contractList;
+	}
+	
 	protected void setup() {
+		this.contractList = new LinkedList<SLAContract>();
 		System.out.println("SlaChecker: "+this.getName());
 		DFUtil.register(this, this.getLocalName(), "sla-checker");
 		this.addBehaviour(new ContextReceiverBehaviour(this));
+		this.addBehaviour(new SLAReceiverBehaviour(this));
 	}
 	
 	protected void takeDown() {
