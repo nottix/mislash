392f5ec5c507d66cab52500df7c17450f517cf00
notargiacomo.s <notargiacomo.s@589bf8a6-3551-0410-881f-c5c050bba8b5>
2008-07-19 13:30:55 +0000

    aggiunti network e bandwidth agli agenti delle risorse

--- SLAsh/src/slash/resource/agent/EnergyAgent.java
+++ SLAsh/src/slash/resource/agent/EnergyAgent.java
@@ -1,6 +1,7 @@
 package slash.resource.agent;
 
 import slash.df.DFUtil;
+import slash.entity.Context;
 import slash.resource.behaviour.*;
 import jade.core.AID;
 import jade.core.Agent;
@@ -9,10 +10,23 @@ public class EnergyAgent extends Agent {
 
 	private static final long serialVersionUID = 7577668356120531127L;
 
+	private int network;
+	private int bandwidth;
+	
 	protected void setup() {
 		System.out.println("Energy: "+this.getName());
 
 		//DFUtil.register(this, this.getLocalName(), "resource");
+		
+		Object[] args = this.getArguments();
+		if(args.length==2) {
+			if(args[0].toString().equals("wired"))
+				network = Context.WIRED;
+			else
+				network = Context.WIRELESS;
+			
+			bandwidth = Integer.parseInt(args[1].toString());
+		}
 		
 		AID cmAid = new AID("cm"+this.getLocalName().charAt(this.getLocalName().length()-1), AID.ISLOCALNAME);
 		this.addBehaviour(new EnergyBehaviour(cmAid));
@@ -21,5 +35,13 @@ public class EnergyAgent extends Agent {
 	
 	protected void takeDown() {
 		//DFUtil.deregister(this, this.getLocalName(), "resource");
+	}
+	
+	public int getNetwork() {
+		return this.network;
+	}
+	
+	public int getBandwidth() {
+		return this.bandwidth;
 	}
 }
