f2f419c5e1473ae801a8c29383b47c9588ceffce
notargiacomo.s <notargiacomo.s@589bf8a6-3551-0410-881f-c5c050bba8b5>
2008-07-19 13:30:55 +0000

    aggiunti network e bandwidth agli agenti delle risorse

--- trunk/SLAsh/src/slash/resource/agent/CpuAgent.java
+++ trunk/SLAsh/src/slash/resource/agent/CpuAgent.java
@@ -3,16 +3,30 @@ package slash.resource.agent;
 import jade.core.AID;
 import jade.core.Agent;
 import slash.df.DFUtil;
+import slash.entity.Context;
 import slash.resource.behaviour.CpuBehaviour;
 
 public class CpuAgent extends Agent {
 
 	private static final long serialVersionUID = -5570077702241336240L;
+
+	private int network;
+	private int bandwidth;
 	
 	protected void setup() {
 		System.out.println("Cpu: "+this.getName());
 
 		//DFUtil.register(this, this.getLocalName(), "resource");
+		
+		Object[] args = this.getArguments();
+		if(args.length==2) {
+			if(args[0].toString().equals("wired"))
+				network = Context.WIRED;
+			else
+				network = Context.WIRELESS;
+			
+			bandwidth = Integer.parseInt(args[1].toString());
+		}
 		
 		AID cmAid = new AID("cm"+this.getLocalName().charAt(this.getLocalName().length()-1), AID.ISLOCALNAME);
 		this.addBehaviour(new CpuBehaviour(cmAid));
@@ -23,5 +37,11 @@ public class CpuAgent extends Agent {
 		//DFUtil.deregister(this, this.getLocalName(), "resource");
 	}
 
-
+	public int getNetwork() {
+		return this.network;
+	}
+	
+	public int getBandwidth() {
+		return this.bandwidth;
+	}
 }
--- trunk/SLAsh/src/slash/resource/agent/LatencyAgent.java
+++ trunk/SLAsh/src/slash/resource/agent/LatencyAgent.java
@@ -2,17 +2,32 @@ package slash.resource.agent;
 
 import slash.df.DFUtil;
 import slash.resource.behaviour.*;
+import slash.resourcemonitor.behaviour.SLAReqReceiverBehaviour;
 import jade.core.AID;
 import jade.core.Agent;
+import slash.entity.*;
 
 public class LatencyAgent extends Agent {
 
 	private static final long serialVersionUID = -5626212945073184408L;
 
+	private int network;
+	private int bandwidth;
+	
 	protected void setup() {
 		System.out.println("Latency: "+this.getName());
 
 		//DFUtil.register(this, this.getLocalName(), "resource");
+		
+		Object[] args = this.getArguments();
+		if(args.length==2) {
+			if(args[0].toString().equals("wired"))
+				network = Context.WIRED;
+			else
+				network = Context.WIRELESS;
+			
+			bandwidth = Integer.parseInt(args[1].toString());
+		}
 		
 		AID rmAid = new AID("rm"+this.getLocalName().charAt(this.getLocalName().length()-1), AID.ISLOCALNAME);
 		this.addBehaviour(new LatencyBehaviour(rmAid));
@@ -21,5 +36,13 @@ public class LatencyAgent extends Agent {
 	
 	protected void takeDown() {
 		//DFUtil.deregister(this, this.getLocalName(), "resource");
+	}
+	
+	public int getNetwork() {
+		return this.network;
+	}
+	
+	public int getBandwidth() {
+		return this.bandwidth;
 	}
 }
