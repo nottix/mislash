820ece87d78a4ddd436bc95711b9efd4e3ed0caa
notargiacomo.s <notargiacomo.s@589bf8a6-3551-0410-881f-c5c050bba8b5>
2008-09-21 12:33:02 +0000

    report

--- SLAsh/doc/graphs/run.sh
+++ SLAsh/doc/graphs/run.sh
@@ -19,12 +19,27 @@ echo "set ylabel \"$YLB\"" >> aux.gnu
 
 PLOT="plot \"$1\" notitle with lines lt 1 linewidth 1"
 TEST=`echo $1 | grep 'index*'`
+TEST1=`echo $1 | grep 'cpu*'`
+TEST2=`echo $1 | grep 'ram*'`
+TEST3=`echo $1 | grep 'energy*'`
 echo $TEST
+echo $TEST1
+echo $TEST2
+OUT="$PLOT"
+echo "out: $OUT"
 if [ "$TEST" != "" ]; then
-	echo "$PLOT, 52 notitle with lines lt 3 linewidth 1" >> aux.gnu
-else
-	echo "$PLOT" >> aux.gnu
+	OUT="$OUT, 52 notitle with lines lt 3 linewidth 1"
 fi
+if [ "$TEST1" != "" ]; then
+	OUT="$OUT, 60 notitle with lines lt 3 linewidth 1"
+fi
+if [ "$TEST2" != "" ]; then
+	OUT="$OUT, 60 notitle with lines lt 3 linewidth 1"
+fi
+if [ "$TEST3" != "" ]; then
+	OUT="$OUT, 10 notitle with lines lt 3 linewidth 1"
+fi
+echo "$OUT" >> aux.gnu
 
 gnuplot script.input
 wait
