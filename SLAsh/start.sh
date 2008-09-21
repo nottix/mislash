69f6bf2e2c7fd8b408837ade20e456716c7a1790
notargiacomo.s <notargiacomo.s@589bf8a6-3551-0410-881f-c5c050bba8b5>
2008-09-21 16:56:00 +0000

    report da rileggere

--- SLAsh/start.sh
+++ SLAsh/start.sh
@@ -1,2 +1,2 @@
-/usr/lib/jvm/java-6-sun-1.6.0.06/jre/bin/java -classpath "./lib/jade.jar;./lib/iiop.jar;./lib/http.jar;./lib/jadeTools.jar;./bin;./" -jar ./lib/jade.jar -gui SC:bin.slash.slachecker.agent.SlaCheckerAgent
+ant -f build2.xml AgentLauncher
 
--- SLAsh/startJade.sh
+++ SLAsh/startJade.sh
@@ -1,2 +1,1 @@
-java -classpath "./lib/jade.jar;./lib/iiop.jar;./lib/http.jar;./lib/jadeTools.jar;./bin;./" jade.Boot -gui jade.Boot
-
+ant runGui
